ee688313a655624e2c6a742f3b49f2127b9073ce
DariaParlea <147147100+DariaParlea@users.noreply.github.com>
2023-10-27 17:33:47 +0300

    entities

---
+++ src/main/java/org/example/model/CartItem.java
@@ -0,0 +1,27 @@
+package org.example.model;
+
+public class CartItem {
+    private Books book;
+    private int quantity;
+
+    public CartItem(Books book, int quantity) {
+        this.book = book;
+        this.quantity = quantity;
+    }
+
+    public Books getBook() {
+        return book;
+    }
+
+    public void setBook(Books book) {
+        this.book = book;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
